652b6e67fc1e97cfcef0b0f564dbfb054f5e1480
YuzuRyo61 <yuzuryo61@yuzulia.work>
2021-09-30 19:08:42 +0900

    Remove never used imports

--- src/app/components/user-card/user-card.component.ts
+++ src/app/components/user-card/user-card.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { MatSnackBar } from "@angular/material/snack-bar";
 import { User } from "../../interface/user";
 import { AuthService } from "../../service/auth.service";
