5de5bbaf0fccc52b1cc42e22c7f70c6fe892285f
YuzuRyo61 <yuzuryo61@yuzulia.work>
2021-10-22 18:46:33 +0900

    Settings page (WIP)

--- src/app/pages/settings/settings.module.ts
+++ src/app/pages/settings/settings.module.ts
@@ -4,6 +4,9 @@ import { SettingsComponent } from "./settings.component";
 import { RouterModule } from "@angular/router";
 import { MatExpansionModule } from "@angular/material/expansion";
 import { MatInputModule } from "@angular/material/input";
+import { ReactiveFormsModule } from "@angular/forms";
+import { MatIconModule } from "@angular/material/icon";
+import { MatButtonModule } from "@angular/material/button";
 
 
 
@@ -19,6 +22,9 @@ import { MatInputModule } from "@angular/material/input";
     }]),
     MatExpansionModule,
     MatInputModule,
+    ReactiveFormsModule,
+    MatIconModule,
+    MatButtonModule,
   ],
   exports: [
     RouterModule,
